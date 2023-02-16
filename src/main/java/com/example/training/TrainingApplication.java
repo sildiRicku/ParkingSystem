1108993e3f252006b905de7920863e57737fcfe0
Sildi <sildi.ricku@albanialab.al>
2023-02-16 17:11:07 +0100

    Initial commit

---
+++ src/main/java/com/example/training/TrainingApplication.java
@@ -0,0 +1,26 @@
+package com.example.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TrainingApplication {
+@GetMapping("/hello")
+public String helloWorld(){
+	return "Hello World";
+}
+@PostMapping("/hellow")
+public String helloWorldi(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return "Hello " + name + "!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingApplication.class, args);
+
+	}
+
+}
