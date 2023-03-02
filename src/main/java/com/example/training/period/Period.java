aaf7f786c8233be53a6823aaf059cda8b75f87f6
sildiRicku <sildi.ricku@albanialab.al>
2023-03-02 10:00:39 +0100

    commit

--- src/main/java/com/example/training/period/Period.java
+++ src/main/java/com/example/training/period/Period.java
@@ -8,7 +8,7 @@ import jakarta.persistence.*;
 public class Period {
     @EmbeddedId
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_periodId")
-    @SequenceGenerator(name="SEQ_perioId", sequenceName = "SEQ_periodId", allocationSize=1)
+    @SequenceGenerator(name="SEQ_periodId", sequenceName = "SEQ_periodId", allocationSize=1)
     private PeriodId periodId;
     @OneToOne(mappedBy = "period")
     private Rule rule;
