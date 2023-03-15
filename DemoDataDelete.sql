8e6e8c6bf14ab2ad5f99efb3069d266f461f9c98
sildiRicku <sildi.ricku@albanialab.al>
2023-03-15 11:06:35 +0100

    sql insert sql delete

--- DemoDataDelete.sql
+++ DemoDataDelete.sql
@@ -1,20 +1,17 @@
 use parkingdb;
 
 #Delete from parking system#
-DELETE FROM parkingdb.parking_system WHERE system_id=1;
-DELETE FROM parkingdb.parking_system WHERE system_id=2;
+DELETE
+FROM parkingdb.parking_system;
 
 #delete rule#
-DELETE FROM parkingdb.rule WHERE rule_id = 1;
-DELETE FROM parkingdb.rule WHERE rule_id = 2;
-DELETE FROM parkingdb.rule WHERE rule_id = 3;
+DELETE
+FROM parkingdb.rule;
 
 #remove rule assignment#
-DELETE FROM parkingdb.parking_system_rule WHERE rule_id = 1;
-DELETE FROM parkingdb.parking_system_rule WHERE rule_id = 2;
-DELETE FROM parkingdb.parking_system_rule WHERE rule_id = 3;
+DELETE
+FROM parkingdb.parking_system_rule;
 
 #remove period assignment to rule#
-DELETE FROM parkingdb.period WHERE period_id = 1;
-DELETE FROM parkingdb.period WHERE period_id = 2;
-DELETE FROM parkingdb.period WHERE period_id = 3;
+DELETE
+FROM parkingdb.period;
