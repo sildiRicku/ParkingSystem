b524556d7cc178b3f70f45ff31938269ace01e8b
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-04-19 15:21:47 +0200

    test calculateExitTime test getExitTime

--- src/test/java/com/example/system/services/RuleServiceTest.java
+++ src/test/java/com/example/system/services/RuleServiceTest.java
@@ -1,69 +1,83 @@
-//package com.example.system.services;
-//
-//import com.example.system.dto.ParkingSystemDTO;
-//import com.example.system.entities.Rule;
-//import com.example.system.entities.TransactionPaymentType;
-//import com.example.system.repositories.RuleRepo;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//import org.modelmapper.ModelMapper;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.*;
-//
-//import java.time.LocalDateTime;
-//import java.time.LocalTime;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//
-//class RuleServiceTest {
-//
-//
-//    @Test
-//    void testCalculateDailyCost() {
-//        RuleRepo ruleRepo = Mockito.mock(RuleRepo.class);
-//        ModelMapper modelMapper = Mockito.mock(ModelMapper.class);
-//        RuleService ruleService = new RuleService(ruleRepo, modelMapper);
-//
-//        ParkingSystemDTO parkingSystemDTO = Mockito.mock(ParkingSystemDTO.class);
-//
-//        Rule rule = mock(Rule.class);
-//        List<Rule> rules = new ArrayList<>();
-//        rules.add(rule);
-//        when(parkingSystemDTO.getRules()).thenReturn(rules);
-//
-//
-//        when(rule.getStartTime()).thenReturn(LocalTime.of(9, 0));
-//        when(rule.getEndTime()).thenReturn(LocalTime.of(18, 0));
-//        when(rule.getCost()).thenReturn(10.0);
-//
-//        double dailyCost = ruleService.calculateDailyCost(rule);
-//        assertEquals(90.0, dailyCost);
-//
-//    }
-//
-//    @Test
-//    void getExitTime() {
-//        RuleRepo ruleRepo = Mockito.mock(RuleRepo.class);
-//        ModelMapper modelMapper = Mockito.mock(ModelMapper.class);
-//        RuleService ruleService = new RuleService(ruleRepo, modelMapper);
-//        ParkingSystemDTO parkingSystemDTO = Mockito.mock(ParkingSystemDTO.class);
-//        Rule rule = Mockito.mock(Rule.class);
-//        String plateNumber = "AA468ZS";
-//
-//        List<Rule> rules = new ArrayList<>();
-//        rules.add(rule);
-//        when(parkingSystemDTO.getRules()).thenReturn(rules);
-//
-//
-//        when(rule.getStartTime()).thenReturn(LocalTime.of(8, 0, 0));
-//        when(rule.getEndTime()).thenReturn(LocalTime.of(20, 0, 0));
-//        when(rule.getCost()).thenReturn(1.0);
-//
-//        String exitTime = ruleService.getExitTime(LocalDateTime.now(), 12.0, plateNumber, parkingSystemDTO, TransactionPaymentType.CASH);
-//
-//        assertEquals("You can park until: 08:00:00 of Date: 2023-04-06", exitTime);
-//    }
-//
-//}
+package com.example.system.services;
+
+import com.example.system.dto.ParkingSystemDTO;
+import com.example.system.entities.Rule;
+import com.example.system.repositories.RuleRepo;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+class RuleServiceTest {
+
+
+    @Test
+    void testCalculateDailyCost() {
+        RuleRepo ruleRepo = Mockito.mock(RuleRepo.class);
+        ModelMapper modelMapper = Mockito.mock(ModelMapper.class);
+        RuleService ruleService = new RuleService(ruleRepo, modelMapper);
+
+        ParkingSystemDTO parkingSystemDTO = Mockito.mock(ParkingSystemDTO.class);
+
+        Rule rule = mock(Rule.class);
+        List<Rule> rules = new ArrayList<>();
+        rules.add(rule);
+        when(parkingSystemDTO.getRules()).thenReturn(rules);
+
+
+        when(rule.getStartTime()).thenReturn(LocalTime.of(9, 0));
+        when(rule.getEndTime()).thenReturn(LocalTime.of(18, 0));
+        when(rule.getCost()).thenReturn(10.0);
+
+        double dailyCost = ruleService.calculateDailyCost(rule);
+        assertEquals(90.0, dailyCost);
+    }
+
+    @Test
+    void calculateExitTime() {
+        LocalDateTime now = LocalDateTime.of(2022, 3, 17, 10, 00, 0);
+        Rule activeRule = mock(Rule.class);
+        when(activeRule.getStartTime()).thenReturn(LocalTime.of(9, 0, 0));
+        when(activeRule.getEndTime()).thenReturn(LocalTime.of(18, 0, 0));
+        int daysToAdd = 2;
+        double secondsRemaining = 3600.0;
+
+        RuleRepo ruleRepo = mock(RuleRepo.class);
+        ModelMapper modelMapper = mock(ModelMapper.class);
+
+        RuleService ruleService = new RuleService(ruleRepo, modelMapper);
+
+        LocalDateTime result = ruleService.calculateExitTime(now, activeRule, daysToAdd, secondsRemaining);
+        LocalDateTime expected = LocalDateTime.of(2022, 3, 19, 11, 0, 0);
+        assertEquals(expected,result);
+    }
+
+
+    @Test
+    void getExitTime() {
+        LocalDateTime now = LocalDateTime.of(2022, 4, 19, 12, 0, 0);
+
+        ParkingSystemDTO parkingSystemDTO = mock(ParkingSystemDTO.class);
+        List<Rule> rules = new ArrayList<>();
+        Rule rule = new Rule();
+        rule.setStartTime(LocalTime.of(9, 0, 0));
+        rule.setEndTime(LocalTime.of(18, 0, 0));
+        rule.setCost(10.0);
+        rules.add(rule);
+        when(parkingSystemDTO.getRules()).thenReturn(rules);
+        RuleService ruleService = mock(RuleService.class);
+        when(ruleService.calculateDailyCost(rule)).thenReturn(80.0);
+        LocalDateTime expectedExitTime = LocalDateTime.of(2022, 4, 20, 15, 0, 0);
+        when(ruleService.calculateExitTime(now, rule, 0, 0.0)).thenReturn(expectedExitTime);
+
+        LocalDateTime actualExitTime = ruleService.calculateExitTime(now, rule, 0, 0.0);
+        assertEquals(expectedExitTime, actualExitTime);
+    }
+
+}
