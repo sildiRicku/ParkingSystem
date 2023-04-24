51df24922e71bca51ff1a3cf094ddfe985688526
sildiRicku <sildi.ricku@albanialab.al>
2023-04-24 11:45:30 +0200

    error handling

--- src/main/java/com/example/system/exceptionhandlers/NotFoundException.java
+++ src/main/java/com/example/system/exceptionhandlers/NotFoundException.java
@@ -1,5 +1,8 @@
 package com.example.system.exceptionhandlers;
 
+import lombok.Data;
+
+@Data
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
