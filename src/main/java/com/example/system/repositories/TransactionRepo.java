56a3cb3a6e3667c3c43dbda5f4f458fe3c6ad09a
Sildi <sildi.ricku@albanialab.al>
2023-09-07 15:33:42 +0200

    fixed

--- src/main/java/com/example/system/repositories/TransactionRepo.java
+++ src/main/java/com/example/system/repositories/TransactionRepo.java
@@ -2,6 +2,15 @@ package com.example.system.repositories;
 
 import com.example.system.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TransactionRepo extends JpaRepository<Transaction, Integer> {
+//    @Query(value = "SELECT * FROM transactions t WHERE t.entry_time >= DATE_SUB(NOW(), INTERVAL 24 HOUR)", nativeQuery = true)
+//    List<Transaction> findTransactionsInLast24Hours();
+    @Query(value = "SELECT * FROM transactions t WHERE t.parking_system_id = :parkingSystemId AND t.entry_time >= DATE_SUB(NOW(), INTERVAL 24 HOUR)", nativeQuery = true)
+    List<Transaction> findTransactionsInLast24HoursForParkingSystem(@Param("parkingSystemId") int parkingSystemId);
+
 }
