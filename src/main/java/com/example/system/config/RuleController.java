628ee119085f261ec0db429fd6cde9f33139a2d6
sildiRicku <sildi.ricku@albanialab.al>
2023-10-04 15:50:00 +0200

    finish

---
+++ src/main/java/com/example/system/config/RuleController.java
@@ -0,0 +1,37 @@
+package com.example.system.config;
+
+import com.example.system.dto.ParkingSystemDTO;
+import com.example.system.models.ParkingSystem;
+import com.example.system.services.ParkingSystemService;
+import com.example.system.services.RuleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rules")
+public class RuleController {
+    private final RuleService ruleService;
+    private final ParkingSystemService parkingSystemService;
+
+    @Autowired
+    public RuleController(RuleService ruleService, ParkingSystemService parkingSystemService) {
+        this.ruleService = ruleService;
+        this.parkingSystemService = parkingSystemService;
+    }
+
+    @GetMapping("/check-rule-conflict/")
+    public ResponseEntity<String> checkRuleConflict(@RequestParam int systemId) {
+
+        ParkingSystemDTO parkingSystem = parkingSystemService.getParkingSystemById(systemId).get();
+
+        boolean hasConflict = ruleService.checkRuleConflict(parkingSystem);
+
+        if (hasConflict) {
+            return ResponseEntity.ok("Rule conflict detected.");
+        } else {
+            return ResponseEntity.ok("No rule conflicts.");
+        }
+    }
+
+}
--- src/main/java/com/example/system/services/RuleService.java
+++ src/main/java/com/example/system/services/RuleService.java
@@ -1,10 +1,13 @@
 package com.example.system.services;
 
+import com.example.system.dto.ParkingSystemDTO;
 import com.example.system.dto.RuleDTO;
 import com.example.system.models.ParkingSystem;
+import com.example.system.models.Rule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
 import java.util.List;
 
 @Service
@@ -16,9 +19,29 @@ public class RuleService {
         this.parkingSystemService = parkingSystemService;
     }
 
-    public boolean checkRuleConflict(ParkingSystem parkingSystem) {
-        List<RuleDTO> rules = parkingSystemService.getRulesForParkingSystem(parkingSystem.getSystemId());
-        return true;
+    public boolean checkRuleConflict(ParkingSystemDTO parkingSystem) {
+        List<RuleDTO> rules = parkingSystemService.getRulesForParkingSystem(parkingSystem.getId());
+        for (int i = 0; i < rules.size(); i++) {
+            for (int j = i + 1; j < rules.size(); j++) {
+                RuleDTO rule1 = rules.get(i);
+                RuleDTO rule2 = rules.get(j);
+
+                if (isTimeRangeOverlap(rule1.getStartTime(), rule1.getEndTime(), rule2.getStartTime(), rule2.getEndTime())) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
+    //    private boolean isTimeRangeOverlap(LocalTime start1, LocalTime end1, LocalTime start2, LocalTime end2) {
+//        return !end1.isBefore(start2) && !end2.isBefore(start1);
+//        //handle the endTime=startTime of next case
+//    }
+    private boolean isTimeRangeOverlap(LocalTime start1, LocalTime end1, LocalTime start2, LocalTime end2) {
+        return !end1.isBefore(start2) && !end2.isBefore(start1) && !end1.equals(start2) && !end2.equals(start1);
+    }
+
+
 }
+
