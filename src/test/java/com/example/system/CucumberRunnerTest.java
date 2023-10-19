afc4e1ea7b3564d1e2f76b95e9b528fa1592180b
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-10-19 15:39:44 +0200

    cucumber tester

---
+++ src/test/java/com/example/system/CucumberRunnerTest.java
@@ -0,0 +1,16 @@
+package com.example.system;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            plugin = {"pretty", "html:target/cucumber-reports/report.html",
+                    "json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+            features = "classpath:features",
+            glue = {"com.example.medicalmanagement.stepDefinitions", "steps"}
+    )
+    public class CucumberRunnerTest {
+
+    }
