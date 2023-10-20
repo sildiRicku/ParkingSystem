0f1cf45f400effb07674c1ec7b36f9306445a450
Klea <kleaprifti21@gmail.com>
2023-10-20 15:51:15 +0200

    commit

--- src/test/java/com/example/system/CucumberRunnerTest.java
+++ src/test/java/com/example/system/CucumberRunnerTest.java
@@ -12,7 +12,6 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = {"com.example.system.stepdefinitions", "steps"}
 )
-@CucumberContextConfiguration
 
 public class CucumberRunnerTest {
 
