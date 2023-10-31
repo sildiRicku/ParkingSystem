03386c745b1cb01036771a4a7eaa9df2dcb1b324
RomeisaAliu <romeisa.aliu@albanialab.al>
2023-10-31 17:28:42 +0100

    serenity miracle

--- src/test/java/com/example/system/CucumberRunnerTest.java
+++ src/test/java/com/example/system/CucumberRunnerTest.java
@@ -1,10 +1,10 @@
 package com.example.system;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber-reports/report.html",
                 "json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
