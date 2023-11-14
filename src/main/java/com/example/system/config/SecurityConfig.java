f37fe45bbbc5ce95c112dc6c35f7e05515781a16
sildiRicku <sildi.ricku@albanialab.al>
2023-11-14 14:26:31 +0100

    fixed configuration

--- src/main/java/com/example/system/config/SecurityConfig.java
+++ src/main/java/com/example/system/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
+                .authorizeHttpRequests(authz -> authz
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults()).csrf(csrf -> csrf.disable());
