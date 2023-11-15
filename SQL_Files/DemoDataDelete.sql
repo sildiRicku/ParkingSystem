88ffba813c8fbac56538727cc909f91dd827b0d9
sildiRicku <sildi.ricku@albanialab.al>
2023-11-15 11:25:39 +0100

    refixed demo data

--- SQL_Files/DemoDataDelete.sql
+++ SQL_Files/DemoDataDelete.sql
@@ -1,19 +1,27 @@
-use parkingdb;
-#delete transactions
-DELETE FROM parkingdb.transactions;
-#remove period assignment to rule#
-DELETE FROM parkingdb.period;
-
+use
+parkingdb;
+#delete transactions#
+DELETE
+FROM parkingdb.transactions;
+#remove period assignment to rule #
+DELETE
+FROM parkingdb.period;
 #remove rule assignment#
-DELETE FROM parkingdb.parking_system_rule;
-
+DELETE
+FROM parkingdb.parking_system_rule;
 #delete rule#
 DELETE
 FROM parkingdb.rule;
-
 #Delete from parking system#
 DELETE
 FROM parkingdb.parking_system;
+#delete admin#
+DELETE
+FROM parkingdb.admin;
+#delete loginCredentials#
+DELETE
+FROM parkingdb.user_login_info;
+
 
 
 
