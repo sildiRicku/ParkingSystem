71093ddcee00f73312975067e15a8894f6d50e45
sildiRicku <sildi.ricku@albanialab.al>
2023-11-20 12:26:21 +0100

    fix master

--- src/main/java/com/example/system/repositories/AdminRepo.java
+++ src/main/java/com/example/system/repositories/AdminRepo.java
@@ -2,7 +2,9 @@ package com.example.system.repositories;
 
 import com.example.system.models.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepo extends JpaRepository<Admin, Integer> {
-    Admin findAdminByEmail(String email);
+    Admin findAdminByLoginInfoEmail(String email);
 }
