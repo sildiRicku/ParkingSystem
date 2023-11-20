e1344d900163c73c6f5eb6045a455322af1b3120
sildiRicku <sildi.ricku@albanialab.al>
2023-11-20 12:22:10 +0100

    fix master

--- SQL_Files/DemoData.sql
+++ SQL_Files/DemoData.sql
@@ -1,10 +1,17 @@
 use parkingdb;
+#insert into login info
+
+INSERT INTO user_login_info (email, password)
+VALUES ('sildiricku3@gmail.com', '$2a$12$94elJ.D8EVzytdGrAYmfxeccjYwN0txkOz7i2pThIbGFtGs9WSDYq');
+INSERT INTO user_login_info (email, password)
+VALUES ('sildiricku4@gmail.com', '$2a$12$ATAaa4.L18PEo4J2A43mKePl3zw2BqEMsYATHJj36vVCsULj/uEwK');
+
 #insert into admin#
 
-INSERT INTO admin (full_Name, email, email_preference)
-VALUES ('Romeisa aliu', 'sildiricku3@gmail.com', 'TEXT');
-INSERT INTO admin (full_Name, email, email_preference)
-VALUES ('Klea Prifti', 'sildiricku4@gmail.com', 'CSV_FILE');
+INSERT INTO admin (full_Name, email_preference, login_info_id)
+VALUES ('Romeisa aliu', 'TEXT', (SELECT id FROM user_login_info WHERE email = 'sildiricku3@gmail.com'));
+INSERT INTO admin (full_Name, email_preference, login_info_id)
+VALUES ('Klea Prifti', 'CSV_FILE', (SELECT id FROM user_login_info WHERE email = 'sildiricku4@gmail.com'));
 
 #insert into parking system#
 
@@ -13,12 +20,12 @@ INSERT INTO parkingdb.parking_system (address, firmware_version, first_install_d
                                       total_money,
                                       working_status, admin_id)
 VALUES ('Main Street', '1.0', '2022-05-10', 'PSYSTEM001', 'never', '1500', 'WORKING',
-        (SELECT id FROM admin WHERE email = 'sildiricku3@gmail.com'));
+        (SELECT id FROM admin WHERE full_name = 'Romeisa Aliu'));
 INSERT INTO parkingdb.parking_system(address, firmware_version, first_install_date, unique_identifier, last_update,
                                      total_money,
                                      working_status, admin_id)
 VALUES ('Downtown', '1.2', '2023-01-01', 'PSYSTEM002', 'never', '3000', 'WORKING',
-        (SELECT id FROM admin WHERE email = 'sildiricku4@gmail.com'));
+        (SELECT id FROM admin WHERE full_name = 'Klea Prifti'));
 
 
 #insert into rule #
