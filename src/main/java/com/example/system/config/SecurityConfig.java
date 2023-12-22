b8d786fdab7b1e8e27a21201b15b1c43c91d5511
Sildi <sildi.ricku@albanialab.al>
2023-12-22 14:52:38 +0100

    final

--- src/main/java/com/example/system/config/SecurityConfig.java
+++ src/main/java/com/example/system/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.system.config;
 
+import com.example.system.security.JwtAuthenticationEntryPoint;
+import com.example.system.security.JwtRequestFilter;
 import com.example.system.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +11,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -22,17 +27,36 @@ public class SecurityConfig {
     private PasswordEncoder passwordEncoder;
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
                 .authorizeHttpRequests(authz -> authz.requestMatchers("/login").authenticated()
-                        .anyRequest().permitAll()
+//                        .anyRequest().permitAll()
 
                 )
                 .httpBasic(withDefaults())
                 .csrf(csrf -> csrf.disable());
+
+        http.csrf().disable()
+                .authorizeHttpRequests(authz -> {
+                    try {
+                        authz.requestMatchers("/authenticate").permitAll().
+                        requestMatchers("/login/login2").authenticated().and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+
+        // this filter is to validate the tokens with every request
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
@@ -44,5 +68,6 @@ public class SecurityConfig {
         auth.userDetailsService(customUserDetailsService)
                 .passwordEncoder(passwordEncoder);
     }
+
 
 }
